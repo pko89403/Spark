4560ab12c89dc51578a8175a9882b14d1a1aea04
강석우 <pko89403@hyundai.com>
2021-02-27 20:40:26 +0900

    push to hdfs

--- AdvancedAnalyticswithSpark/ch8/get_data.sh
+++ AdvancedAnalyticswithSpark/ch8/get_data.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 
+# download file
 mkdir taxidata;
 cd taxidata
 wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2013-01.csv
 head -n 10 yellow_tripdata_2013-01.csv
+# put hdfs dfs
+hdfs dfs -mkdir -p taxidata
+hdfs dfs -put yellow_tripdata_2013-01.csv taxidata
